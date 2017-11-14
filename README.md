e35a3f22179fe3fd55ba2d6e2886a1e74939135d
Jano Rozbora <jan.rozbora@gmail.com>
2017-11-14 18:18:38 +0100

    Doplnenie autentifikacie v Laraveli, uprava navigacie

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Twitter
+
+Requirements
+
+git clone
+composer install
+premenovat .env.example to .env
+php artisan key:generate
+
+v .env nakonfigurovat databasu
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="/">Twitter</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        @if(Auth::check())
+          <li class="nav-item">
+          <a class="nav-link" href="#">Nastenka</a>
+        </li>
+        <li class="dropdown nav-item">
+          <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Settings</a></li>
+            <li><a href="/logout">Log out</a></li>
+          </ul>
+        </li>
+        @else
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Log in</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/register">Sign up</a>
+        </li>
+        @endif
+      </ul>
+    </div>
+  </div>
+</nav>
